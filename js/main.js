59998ba282e3491b337f51f51578c81a607e54ca
musella95 <musellamatteoo@hotmail.com>
2023-11-13 15:58:31 +0100

    adjust sale

--- js/main.js
+++ js/main.js
@@ -10,22 +10,21 @@ age = parseInt(age);
 
 //elaborazione prezzo ticket
 
-let priceTicket = (kmTraveled * 0.21);
+let ticketPrice = (kmTraveled * 0.21);
 
-let finalPrice = priceTicket.toFixed(2);
+
 
 //sezione degli if 
 // se l'acquirente ha meno di 18 anni ha uno sconto del 20%
 // se l'acquirente è over 65 ha uno sconto del 40%
-if (age < 18){
-    priceTicket- (priceTicket*0.20)
+if (age < 18) {
+    ticketPrice = ticketPrice * 0.8;
 }
-else if (age >=65){
-    priceTicket-(priceTicket*0.60)
+else if (age >= 65) {
+    ticketPrice = ticketPrice * 0.6
+        ;
 }
-else {
-    priceTicket = priceTicket
-};
+let finalPrice = ticketPrice.toFixed(2);
 
 console.log("il prezzo del biglietto e di €"+ finalPrice);
-document.getElementById("finalPrice").innerHTML = finalPrice
+document.getElementById("biglietto").innerHTML = finalPrice;
