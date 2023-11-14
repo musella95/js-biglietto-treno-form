13c8bbdfba494511af3bfa89bfcf1a32bbe24cf6
musella95 <musellamatteoo@hotmail.com>
2023-11-14 20:24:42 +0100

    finish js

--- js/main.js
+++ js/main.js
@@ -9,11 +9,23 @@
     let surname = document.getElementById("inputSurnameUser").value;
     let km =document.getElementById("inputKmUser").value;
     let age =document.getElementById("inputAge").value;
+    let ticketPrice= (km*0.21);
+    let finalPrice = ticketPrice.toFixed(2);
+    if (age==="underAge") {
+    finalPrice = ticketPrice * 0.8;
+    }
+    else if (age==="overAge") {
+        finalPrice = ticketPrice * 0.6;
+        console.log("sono overage")
+    }
 
-    console.log(name)
-    console.log(surname)
-    console.log(km)
-    console.log(age)
+
+     document.getElementById("passengerName").innerHTML=name;
+     document.getElementById("passengerSurname").innerHTML=surname;
+     document.getElementById("distanceKm").innerHTML=km;
+     document.getElementById("ticketCode").innerHTML=Math.floor(Math.random() * 100001);
+     document.getElementById("ticketPrize").innerHTML= finalPrice;
+
  })
 
  
