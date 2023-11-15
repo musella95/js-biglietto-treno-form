2897fcd33091cfc7a85c4e6e24e9dc748d70c377
musella95 <musellamatteoo@hotmail.com>
2023-11-15 09:48:01 +0100

    revisione con bryan

--- js/main.js
+++ js/main.js
@@ -3,20 +3,25 @@
  let submitBtn = document.getElementById("submitBtn");
  
  
- submitBtn.addEventListener("click",function(){
-    event.preventDefault()
+ submitBtn.addEventListener("click",function(event){
+    // mi ricordo di disabilitare 
+    event.preventDefault();
     let name = document.getElementById("inputNameUser").value;
     let surname = document.getElementById("inputSurnameUser").value;
-    let km =document.getElementById("inputKmUser").value;
+    let km = document.getElementById("inputKmUser").value;
+
+    km=parseInt(km);
+    
+
     let age =document.getElementById("inputAge").value;
     let ticketPrice= (km*0.21);
     let finalPrice = ticketPrice.toFixed(2);
-    if (age==="underAge") {
+    if (age =="underAge") {
     finalPrice = ticketPrice * 0.8;
     }
-    else if (age==="overAge") {
+    else if (age =="overAge") {
         finalPrice = ticketPrice * 0.6;
-        console.log("sono overage")
+        console.log("sono overage");
     }
 
 
